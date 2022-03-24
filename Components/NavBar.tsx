e3e09ca2d0c6a536033c50b905b542fef6766b3b
Ari Shoham <arishoham@gmail.com>
2022-03-23 21:19:32 -0400

    commit before adding mdx

--- Components/NavBar.tsx
+++ Components/NavBar.tsx
@@ -1,12 +1,28 @@
 import React, { useState, useEffect } from "react";
 import styles from "../styles/NavBar.module.scss";
 import LogoSVG from "./LogoSVG";
-import { Button } from "@mantine/core";
+import { Button, Group } from "@mantine/core";
 import Link from "next/link";
 import { motion } from "framer-motion";
+import useSWR from "swr";
+import { AiOutlineStar } from "react-icons/ai";
+
+const fetcher = async (
+  input: RequestInfo,
+  init: RequestInit,
+  ...args: any[]
+) => {
+  const res = await fetch(input, init);
+  return res.json();
+};
 
 const NavBar = ({ scrollYProgress }) => {
   const [isStarted, setIsStarted] = useState(false);
+  const { data, error } = useSWR(
+    "https://api.github.com/repos/oslabs-beta/Vantage",
+    fetcher
+  );
+
   useEffect(
     () => scrollYProgress.onChange((v) => setIsStarted(v > 0)),
     [scrollYProgress]
@@ -28,9 +44,14 @@ const NavBar = ({ scrollYProgress }) => {
       </Link>
 
       <div className={styles.containerRight}>
-        <Button variant='outline' color='violet'>
-          <a href='https://github.com/oslabs-beta/Vantage'>Github</a>
-        </Button>
+        <a href='https://github.com/oslabs-beta/Vantage'>
+          <Button variant='outline' color='violet' className={styles.github}>
+            Github
+            <div>
+              <AiOutlineStar /> {data?.stargazers_count}
+            </div>
+          </Button>
+        </a>
         <Button variant='outline' color='violet'>
           <Link href='/vantage_report_sample.html'>
             <a>Example</a>
