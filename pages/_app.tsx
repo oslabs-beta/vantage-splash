6698578b266cfb82543e96a2213cd359d9ca210b
Ari Shoham <arishoham@gmail.com>
2022-03-31 15:23:05 -0400

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,28 @@
+import Head from "next/head";
 import "nextra-theme-docs/style.css";
 import "../styles/globals.scss";
 
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-ZCQ52NTSKF`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZCQ52NTSKF', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </Head>
       <Component {...pageProps} />
     </>
   );
